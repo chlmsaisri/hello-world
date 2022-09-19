971d3f590b6f8bce35cfe0aa5e726eb2eb85a921
chlmsaisri <81314771+chlmsaisri@users.noreply.github.com>
2022-09-19 16:10:34 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#Add implementation
+def add(x,y):
+	return x+y
+#subtract implementation
+def subtract(x,y):
+	pass
+#multiply implementation
+def multiply(x,y):
+	pass
+#div implementation
+def divide(x,y):
+	pass
